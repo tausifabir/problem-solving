786ec7c1a678e4c7cf8036299b36b3c3b1aa0c27
ABiR <tausifabir94@gmail.com>
2024-01-21 13:32:12 +0600

    style: checky style

--- src/solutions/ContainsDuplicateLeetCode.java
+++ src/solutions/ContainsDuplicateLeetCode.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 
 public class ContainsDuplicateLeetCode {
 
-    /*
+  /*
       Given an integer array nums, return true
       if any value appears at least twice in the array,
       and return false if every element is distinct.
@@ -19,38 +19,43 @@ public class ContainsDuplicateLeetCode {
       * Input: nums = [1,1,1,3,3,4,3,2,4,2]
       * Output: true
     */
-    public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if (numberMap.containsValue(nums[i])) {
-                return true;
-            }
-            numberMap.put(nums[i], nums[i]);
-        }
-        return false;
+
+  /** containsDuplicate. */
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> numberMap = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (numberMap.containsValue(nums[i])) {
+        return true;
+      }
+      numberMap.put(nums[i], nums[i]);
     }
 
-    public boolean containsDuplicateBeginnerApproach(int[] nums) {
+    return false;
+  }
 
-        for (int i = 0; i <= nums.length; i++) {
-            for(int j = i+1; j <= nums.length-1; j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
-            }
+  /** containsDuplicateBeginnerApproach. */
+  public boolean containsDuplicateBeginnerApproach(int[] nums) {
+
+    for (int i = 0; i <= nums.length; i++) {
+      for (int j = i + 1; j <= nums.length - 1; j++) {
+        if (nums[i] == nums[j]) {
+          return true;
         }
-        return false;
+      }
+    }
+    return false;
+  }
+
+  /** containsDuplicateSetApproach. */
+  public boolean containsDuplicateSetApproach(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int num : nums) {
+      if (!set.add(num)) {
+        return true;
+      }
     }
 
-    public boolean containsDuplicateSetApproach(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            if(!set.add(nums[i])){
-                return true;
-            }
-        }
-
-        return false;
-    }
+    return false;
+  }
 
 }
--- src/solutions/GreatestCommonDivisorOfStrings.java
+++ src/solutions/GreatestCommonDivisorOfStrings.java
@@ -2,10 +2,11 @@ package solutions;
 
 public class GreatestCommonDivisorOfStrings {
 
-    /*
+  /*
      For two strings s and t, we say "t divides s" if
      and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
-     Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+     Given two strings str1 and str2, return the largest string x such
+     that x divides both str1 and str2.
 
      Example 1:
      ------------------------------
@@ -27,74 +28,83 @@ public class GreatestCommonDivisorOfStrings {
      Input: str1 = "LEET", str2 = "CODE"
      Output: ""
      ------------------------------
+     */
 
-    */
+  // TODO: this approach is not working
 
-    // TODO: this approach is not working
-    public String gcdOfStrings(String str1, String str2) {
+  /**
+   * gcdOfStrings.
+   */
+  public String gcdOfStrings(String str1, String str2) {
 
 
-        String firstTwoCharacter = getFirstTwoCharacterString(str1,str2);
+    String firstTwoCharacter = getFirstTwoCharacterString(str1, str2);
 
-        if(str1.length() %2 == 0 && str2.length() %2 == 0){
+    if (str1.length() % 2 == 0 && str2.length() % 2 == 0) {
 
-            if(str1.length() < str2.length()){
-                for(int i = 2; i < str1.length(); i +=2){
-                    if(!firstTwoCharacter.equals(str1.substring(0,2))){
-                        return "";
-                    };
-                }
-
-                for(int i = 0; i<str2.length(); i +=2){
-                    if(!firstTwoCharacter.equals(str2.substring(0,i+2))){
-                        return "";
-                    };
-                }
-            } else if (str2.length() < str1.length()) {
-                for(int i = 2; i < str2.length(); i +=2){
-                    if(!firstTwoCharacter.equals(str2.substring(0,2))){
-                        return "";
-                    };
-                }
-
-                for(int i = 0; i < str1.length(); i +=2){
-                    if(!firstTwoCharacter.equals( str1.substring(i,i+2) )){
-                        return "";
-                    };
-                }
-            }
-
-        }else {
+      if (str1.length() < str2.length()) {
+        for (int i = 2; i < str1.length(); i += 2) {
+          if (!firstTwoCharacter.equals(str1.substring(0, 2))) {
             return "";
+          }
+          ;
         }
 
-        return firstTwoCharacter;
-    }
-
-    private String getFirstTwoCharacterString(String str1, String str2){
-        if(str2.length() < str1.length()){
-            return str2.substring(0,2);
-        }else{
-           return str1.substring(0,2);
-        }
-    }
-
-
-    public String gcdOfStringsV2(String str1, String str2) {
-        if(str1.length() < str2.length()){
-            gcdOfStringsV2(str2,str1);
-        }else if(!str1.startsWith(str2)){
+        for (int i = 0; i < str2.length(); i += 2) {
+          if (!firstTwoCharacter.equals(str2.substring(0, i + 2))) {
             return "";
-        }else if (str2.isEmpty()) {
-            return str1;
+          }
+          ;
         }
-        return gcdOfStringsV2(str2,mod(str1,str2));
+      } else if (str2.length() < str1.length()) {
+        for (int i = 2; i < str2.length(); i += 2) {
+          if (!firstTwoCharacter.equals(str2.substring(0, 2))) {
+            return "";
+          }
+          ;
+        }
+
+        for (int i = 0; i < str1.length(); i += 2) {
+          if (!firstTwoCharacter.equals(str1.substring(i, i + 2))) {
+            return "";
+          }
+          ;
+        }
+      }
+
+    } else {
+      return "";
     }
 
-    private String mod(String s1, final String s2) {
-        while (s1.startsWith(s2)){
-            s1 = s1.substring(s2.length());
-        }
-        return s1;
+    return firstTwoCharacter;
+  }
+
+  private String getFirstTwoCharacterString(String str1, String str2) {
+    if (str2.length() < str1.length()) {
+      return str2.substring(0, 2);
+    } else {
+      return str1.substring(0, 2);
     }
+  }
+
+  /**
+   * gcdOfStringsV2.
+   */
+  public String gcdOfStringsV2(String str1, String str2) {
+    if (str1.length() < str2.length()) {
+      gcdOfStringsV2(str2, str1);
+    } else if (!str1.startsWith(str2)) {
+      return "";
+    } else if (str2.isEmpty()) {
+      return str1;
+    }
+    return gcdOfStringsV2(str2, mod(str1, str2));
+  }
+
+  private String mod(String s1, final String s2) {
+    while (s1.startsWith(s2)) {
+      s1 = s1.substring(s2.length());
+    }
+    return s1;
+  }
 }
