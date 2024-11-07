d29ea2bb4ccc5b5bca3d54151c0416c6e2bb5aff
tausifabir <tausif.islam@bracits.com>
2024-11-07 17:49:27 +0600

    feat: Leet Code 347. Top K Frequent Elements solved

--- src/solutions/TopKFrequent.java
+++ src/solutions/TopKFrequent.java
@@ -1,6 +1,7 @@
 package solutions;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class TopKFrequent {
 
@@ -35,6 +36,8 @@ public class TopKFrequent {
 
   */
 
+  // Runtime : 147ms, Memory : 47MB.
+  /** primary thoughtful process 1st approach.*/
   public int[] topKFrequent(int[] nums, int k) {
 
     if (nums.length == 0) {
@@ -43,6 +46,7 @@ public class TopKFrequent {
 
     int [] returnArray = new int[k];
     HashMap<Integer, Integer>  givenUniqueNumber = new HashMap<>();
+    ArrayList<Integer> list = new ArrayList<>();
 
 
     for (int num : nums) {
@@ -53,15 +57,38 @@ public class TopKFrequent {
     }
 
     for (int num : nums) {
-      if(givenUniqueNumber.containsKey(num)) {
+      if (givenUniqueNumber.containsKey(num)) {
         givenUniqueNumber.put(num,givenUniqueNumber.get(num)+1);
+        System.out.println(num+ " - " + givenUniqueNumber.get(num));
       }
     }
 
-    int index = 0;
-    while (index < k) {
-      returnArray [index] = givenUniqueNumber.get();
-      index++;
+
+    HashMap<Integer, Integer> sortedMap = givenUniqueNumber.entrySet()
+        .stream()
+        .sorted((i1, i2) -> i2.getValue().compareTo(i1.getValue())) // Note the order is reversed
+        .collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue,
+            (e1, e2) -> e1,
+            LinkedHashMap::new));
+
+    int i = 0;
+
+    // print the sorted hashmap
+    for (Map.Entry<Integer, Integer> en :
+            sortedMap.entrySet()) {
+
+
+      System.out.println("Key = " + en.getKey()
+              + ", Value = "
+              + en.getValue());
+      returnArray[i] = en.getKey();
+      i++;
+
+      if(i>=k) {
+        break;
+      }
     }
 
     return returnArray;
