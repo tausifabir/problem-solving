45abf955d23352732de4019740798b33bbf32c82
tausif.islam <tausif.islam@bracits.com>
2026-01-19 17:57:47 +0600

    feat: Leet Code 15. 3Sum

--- src/solutions/ThreeSum.java
+++ src/solutions/ThreeSum.java
@@ -127,4 +127,55 @@ public class ThreeSum {
 
         return result;
     }
+
+
+
+    /**
+     * best solution.
+     */
+    // Runtime : 28s, Memory : 60MB.
+    public List<List<Integer>> threeSumV2(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums.length < 3) return result;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Early exit if nums[i] is positive
+            if (nums[i] > 0) break;
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    // Add triplet to result
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Move left pointer past duplicates
+                    int leftValue = nums[left];
+                    while (left < right && nums[left] == leftValue) left++;
+
+                    // Move right pointer past duplicates
+                    int rightValue = nums[right];
+                    while (left < right && nums[right] == rightValue) right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
